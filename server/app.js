4da3d2d30368cc36234ea09f4f7bc6fc97317b82
Laura Patterson <laura.patterson53@hotmail.co.uk>
2023-08-14 11:48:30 +0100

    Q2

---
+++ server/app.js
@@ -0,0 +1,21 @@
+const { getTopics } = require("./controller");
+
+const express = require("express");
+const app = express();
+
+app.get("/api/topics", getTopics);
+
+app.use((err, req, res, next) => {
+  if (err.code === "404") {
+    res.status(404).send({ msg: "path not found" });
+  }
+  else{
+    next(err)
+  }
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).send("Server Error!");
+});
+
+module.exports = app;
---
+++ server/controller.js
@@ -0,0 +1,10 @@
+const {selectTopics} = require("./model")
+
+exports.getTopics = (req,res) =>{
+    selectTopics().then((topics)=>{
+        res.status(200).send(topics)
+    }).catch((err)=>{
+        console.log("here")
+        next(err)
+    })
+}
