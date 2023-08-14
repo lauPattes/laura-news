53f3ac6848d440ea40a8e80e1d96c0fe5950f2f8
Laura Patterson <laura.patterson53@hotmail.co.uk>
2023-08-14 15:45:01 +0100

    Q3 complete

--- __tests__/endpoint.test.js
+++ __tests__/endpoint.test.js
@@ -3,6 +3,8 @@ const request = require("supertest");
 const db = require("../db/connection");
 const seed = require("../db/seeds/seed");
 const testData = require("../db/data/test-data/index");
+const endpoints = require("../endpoints.json")
+const fs = require("fs/promises")
 
 beforeEach(() => seed(testData));
 
@@ -26,6 +28,22 @@ describe("/api/topics", () => {
       });
   });
   test("Get 404 sends an appropriate error message when given an incorrect path", () => {
-    return request(app).get("/api/topics/23").expect(404);
+    return request(app).get("/api/topicZ").expect(404).then((response)=>{
+        console.log(response.body.msg)
+        expect(response.body.msg).toEqual("path not found")
+    });
   });
 });
+
+describe("/api",()=>{
+    test("Get 200 sends object to the client describing all the endpoints available",()=>{
+        return request(app).get("/api")
+        .expect(200)
+        .then((response)=>{
+            const gotFile = response.body.endpoints
+            return fs.readFile("endpoints.json","utf8").then((readFile)=>{
+                expect(gotFile).toEqual(JSON.parse(readFile))
+            })
+        })
+    })
+})
--- server/model.js
+++ server/model.js
@@ -1,8 +1,15 @@
 const db = require("../db/connection")
+const endpoints = require("../endpoints.json")
+const fs = require("fs/promises")
 
 exports.selectTopics = () =>{
     return db.query('SELECT * FROM topics;').then((result)=>{
-        // console.log(result.rows)
         return result.rows
+    })
+}
+
+exports.selectEndpoints = ()=>{
+    return fs.readFile("endpoints.json","utf8").then((response)=>{
+        return JSON.parse(response)
     })
 }
